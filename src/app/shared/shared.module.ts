ea092a4d749046810fe19c6c62498205f3731c01
fin-framilano <francesco.milano@finconsgroup.com>
2022-05-26 16:59:43 +0200

    Added search and list todos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommonTableComponent } from './components/common-table/common-table.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CommonTableComponent],
   imports: [
     CommonModule
-  ]
+  ], exports: [CommonTableComponent]
 })
 export class SharedModule { }
